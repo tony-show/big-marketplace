62cb7a616f018245a15b1a1a82c27f602c12302a
Anatoly Ivashov <tonyshow2011@gmail.com>
2022-07-27 12:07:18 +0200

    Main page and base main styles

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -2,8 +2,12 @@ import { ReactFC } from '@src/interfaces/react'
 import React from 'react'
 
 const MainPage: ReactFC = () => {
-  return <div className="wrapper">
-    <h1>Hello!</h1>
-  </div>
+  return (
+    <div className='wrapper'>
+      <h1>
+        <i className='ic_search' />
+      </h1>
+    </div>
+  )
 }
 export default MainPage
