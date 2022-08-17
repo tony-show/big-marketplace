7a04e447e80454fbf07f881e6f89880bdd7e5664
Anatoly Ivashov <tonyshow2011@gmail.com>
2022-08-17 15:10:39 +0200

    add: template component

---
+++ src/components/template/template.tsx
@@ -0,0 +1,8 @@
+import { ReactFC } from '@src/interfaces/react'
+import React from 'react'
+import './template.sass'
+
+const Template: ReactFC = () => {
+  return <div className='' />
+}
+export default Template
