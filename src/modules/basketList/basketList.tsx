81ce1e4e809e8b856c443512013966ea7b78171c
Anatoly Ivashov <tonyshow2011@gmail.com>
2022-09-07 10:37:20 +0200

    Add orders list component for Basket page

---
+++ src/modules/basketList/basketList.tsx
@@ -0,0 +1,93 @@
+import functionHelpers from '@src/helpers/functionHelpers'
+import { IBasketProduct } from '@src/interfaces/product'
+import { ReactFC } from '@src/interfaces/react'
+import React, { useState } from 'react'
+import { Form } from 'react-bootstrap'
+import './basketList.sass'
+import BasketListItem from './basketListItem/basketListItem'
+
+interface IBasketListProps {
+  products: IBasketProduct[]
+  changeCount: (id: number, count: number) => void
+  onDelete: (id: number) => void
+  toFavorite: (id: number) => void
+  onChecked: (id: number, checked: boolean) => void
+  checkedAll: boolean
+  onCheckedAll: (checkedAll: boolean) => void
+}
+
+const BasketList: ReactFC<IBasketListProps> = ({
+  products,
+  changeCount,
+  onDelete,
+  toFavorite,
+  onChecked,
+  checkedAll,
+  onCheckedAll,
+}) => {
+  const [isOpen, setIsOpen] = useState(true)
+
+  const onChangeCheckedAll = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onCheckedAll(e.target.checked)
+  }
+
+  const renderStatInfo = () => {
+    const initialStat = {
+      count: 0,
+      price: 0,
+    }
+    const statInfo = products.reduce((total, item) => {
+      return {
+        count: total.count + item.count,
+        price:
+          total.price + functionHelpers.getSalePrace(item.price, item.sale),
+      }
+    }, initialStat)
+    return (
+      <div className='basket-list__stat'>
+        {statInfo.count} товара -{' '}
+        {functionHelpers.getDigitNumber(statInfo.price)} ₽
+      </div>
+    )
+  }
+
+  return (
+    <div className='basket-list'>
+      <div className='basket-list__head'>
+        <div className='basket-list__check-all'>
+          {isOpen && (
+            <Form.Check
+              type='checkbox'
+              id='basket-check-all'
+              label='Выбрать все'
+              checked={checkedAll}
+              onChange={onChangeCheckedAll}
+            />
+          )}
+          {!isOpen && renderStatInfo()}
+          <div
+            className='basket-list__close'
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {isOpen ? '-' : '+'}
+          </div>
+        </div>
+      </div>
+      <div className={`basket-list__products ${isOpen ? '' : 'hide'}`}>
+        {!!products.length &&
+          products.map((product) => (
+            <BasketListItem
+              onDelete={onDelete}
+              toFavorite={toFavorite}
+              key={product.id}
+              product={product}
+              changeCount={changeCount}
+              onChacked={onChecked}
+            />
+          ))}
+        {!products.length && <h3>Корзина пуста</h3>}
+      </div>
+    </div>
+  )
+}
+export default BasketList
---
+++ src/modules/basketList/basketListItem/basketListItem.tsx
@@ -0,0 +1,85 @@
+import SelectCount from '@src/components/selectCount/selectCount'
+import functionHelpers from '@src/helpers/functionHelpers'
+import { ColorsEnum, IBasketProduct } from '@src/interfaces/product'
+import { ReactFC } from '@src/interfaces/react'
+import React from 'react'
+import { Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import './basketListItem.sass'
+
+interface IBasketListItemProps {
+  product: IBasketProduct
+  toFavorite: (id: number) => void
+  onDelete: (id: number) => void
+  changeCount: (id: number, count: number) => void
+  onChacked: (id: number, checked: boolean) => void
+}
+
+const BasketListItem: ReactFC<IBasketListItemProps> = ({
+  product,
+  toFavorite,
+  onDelete,
+  changeCount,
+  onChacked,
+}) => {
+  const getPriceWithSale = () => {
+    const priceWithSale = functionHelpers.getSalePrace(
+      product.price * product.count,
+      product.sale
+    )
+    return functionHelpers.getDigitNumber(priceWithSale)
+  }
+  const getFullPrice = () => {
+    const fullPrice = product.price * product.count
+    return functionHelpers.getDigitNumber(fullPrice)
+  }
+
+  const changeChecked = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChacked(product.id, e.target.checked)
+  }
+
+  return (
+    <div className='basket-product'>
+      <Form.Check
+        type='checkbox'
+        onChange={changeChecked}
+        checked={product.checked}
+      />
+      <Link to={`/product/${product.id}`}>
+        <img
+          src={product.cover}
+          alt={product.name}
+          className='basket-product__img'
+        />
+      </Link>
+      <div className='basket-product__info'>
+        <Link to={`/product/${product.id}`} className='basket-product__title'>
+          {product.name} / {product.ram} / {product.ssd}
+        </Link>
+        {product.selectedColor && (
+          <span>Цвет: {ColorsEnum[product.selectedColor]}</span>
+        )}
+        {product.warehouse && <span>{product.warehouse}</span>}
+      </div>
+      <div className='basket-product__actions'>
+        <SelectCount
+          min={1}
+          initial={product.count}
+          onChange={(count) => {
+            changeCount(product.id, count)
+          }}
+        />
+        <div className='basket-product__links'>
+          <span onClick={() => toFavorite(product.id)}>В избранное</span>
+          <span onClick={() => onDelete(product.id)}>Удалить</span>
+        </div>
+      </div>
+      <div className='basket-product__price'>
+        <strong>{getPriceWithSale()} ₽</strong>
+        <span>{getFullPrice()} ₽</span>
+        <div>рассрочка 1 956 ₽/мес</div>
+      </div>
+    </div>
+  )
+}
+export default BasketListItem
