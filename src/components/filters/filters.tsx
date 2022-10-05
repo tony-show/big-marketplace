da5671c2485cf31e29cc95cc014ecb9b033852c5
Anatoly Ivashov <tonyshow2011@gmail.com>
2022-10-05 20:32:12 +0200

    update: Filters component
    + Connect to products store
    + Added change filters logic

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -1,7 +1,10 @@
 import { ReactFC } from '@src/interfaces/react'
 import React from 'react'
-import Filter, { ICheckRadio } from '../filter/filter'
+import { useAppDispatch, useAppSelector } from '@src/hooks/redux'
+import { ICheckRadio, IColor, IRange } from '@src/interfaces/filters'
+import Filter from '../filter/filter'
 import './filters.sass'
+import { setFilters } from '@src/store/productsStore/productsStore'
 
 const sales: ICheckRadio[] = [
   { id: 1, label: 'от 10% и выше', value: 10 },
@@ -9,34 +12,76 @@ const sales: ICheckRadio[] = [
   { id: 3, label: 'от 50% и выше', value: 50 },
   { id: 4, label: 'от 70% и выше', value: 70 },
 ]
-const brends: ICheckRadio[] = [
-  { id: 1, label: 'Lenovo', value: 'lenovo' },
-  { id: 2, label: 'Acer', value: 'acer' },
-  { id: 3, label: 'HP', value: 'hp' },
-  { id: 4, label: 'Asus', value: 'asus' },
-  { id: 5, label: 'MSI', value: 'msi' },
-  { id: 6, label: 'Apple', value: 'apple' },
-  { id: 7, label: 'Honor', value: 'honor' },
-  { id: 8, label: 'Dell', value: 'dell' },
-  { id: 9, label: 'Dota', value: 'dota' },
-  { id: 10, label: 'Huawei', value: 'huawei' },
-  { id: 11, label: 'Irbis', value: 'irbis' },
-  { id: 12, label: 'Logitech', value: 'logitech' },
-  { id: 13, label: 'Redmi', value: 'redmi' },
-  { id: 14, label: 'Samsung', value: 'samsung' },
-]
 
 const Filters: ReactFC = () => {
+  const dispatch = useAppDispatch()
+  const { filters, selectedFilters } = useAppSelector((state) => state.products)
+
+  const changeColor = (color: IColor) => {
+    const { colors } = selectedFilters
+    const isSelected = colors.includes(color)
+    const withoutSelectedColor = colors.filter((c) => c !== color)
+    dispatch(
+      setFilters({
+        ...selectedFilters,
+        colors: isSelected ? withoutSelectedColor : [...colors, color],
+      })
+    )
+  }
+
+  const changePrice = (price: IRange) => {
+    dispatch(
+      setFilters({
+        ...selectedFilters,
+        price,
+      })
+    )
+  }
+
+  const changeSale = (sale: number) => {
+    dispatch(
+      setFilters({
+        ...selectedFilters,
+        sale,
+      })
+    )
+  }
+
+  const changeBrend = (brend: ICheckRadio) => {
+    const { brends } = selectedFilters
+    const isSelected = brends.some((b) => b.value === brend.value)
+    const withoutSelectedBrend = brends.filter((b) => b.value !== brend.value)
+    dispatch(
+      setFilters({
+        ...selectedFilters,
+        brends: isSelected ? withoutSelectedBrend : [...brends, brend],
+      })
+    )
+  }
+
   return (
     <div className='filters'>
       <Filter
         label='Цвет'
         type='color'
-        data={['black', 'blue', 'yellow', 'red', 'green']}
+        data={filters.colors}
+        selectedData={selectedFilters.colors}
+        onChange={changeColor}
       />
-      <Filter label='Цена' type='range' data={{ from: 100, to: 20000 }} />
-      <Filter label='Скидка' type='radio' data={sales} />
-      <Filter label='Бренды' type='checkbox' data={brends} />
+      <Filter
+        label='Цена'
+        type='range'
+        data={filters.price}
+        onChange={changePrice}
+      />
+      <Filter label='Скидка' type='radio' data={sales} onChange={changeSale} />
+      <Filter
+        label='Бренды'
+        type='checkbox'
+        data={filters.brends}
+        selectedData={selectedFilters.brends}
+        onChange={changeBrend}
+      />
     </div>
   )
 }
