4c0b459127d5e330d6f31db0c87461a0c1b89dc7
Anatoly Ivashov <tonyshow2011@gmail.com>
2022-10-20 17:56:40 +0200

    include user store to favorite page

--- src/modules/favorite/favorite.tsx
+++ src/modules/favorite/favorite.tsx
@@ -1,27 +1,31 @@
-import generateProducts from '@src/data/products'
 import sortHelpers from '@src/helpers/sortHelpers'
+import { useAppSelector } from '@src/hooks/redux'
 import { ColorsEnum } from '@src/interfaces/product'
 import { ReactFC } from '@src/interfaces/react'
 import SortTypeEnum from '@src/interfaces/sort'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './favorite.sass'
 import FavoriteActions from './favoriteActions/favoriteActions'
 import FavoriteCard from './favoriteCard/favoriteCard'
 
-const productsData = generateProducts(30)
-
 const Favorite: ReactFC = () => {
-  const [products, setProducts] = useState(productsData)
-  const [filteredProducts, setFilteredProducts] = useState(productsData)
+  const { favorite } = useAppSelector((state) => state.user)
+  const [filteredProducts, setFilteredProducts] = useState(favorite)
+  const [sortType, setSortType] = useState(SortTypeEnum.addDateUp)
 
   const onSort = (type: SortTypeEnum) => {
-    const sortedProducts = sortHelpers.sortFavoriteProducts(type, products)
+    setSortType(type)
+    const sortedProducts = sortHelpers.sortFavoriteProducts(type, [...favorite])
     setFilteredProducts(sortedProducts)
   }
 
+  useEffect(() => {
+    onSort(sortType)
+  }, [favorite])
+
   const onSearch = (value: string) => {
     const reg = new RegExp(value, 'i')
-    const searchedProducts = products.filter((product) => {
+    const searchedProducts = favorite.filter((product) => {
       return (
         reg.test(product.name) ||
         reg.test(product.brend.label) ||
@@ -31,30 +35,13 @@ const Favorite: ReactFC = () => {
     setFilteredProducts(searchedProducts)
   }
 
-  const deleteFromFavorite = (id: number) => {
-    const filtered = products.filter((product) => product.id !== id)
-    setProducts(filtered)
-    setFilteredProducts(filtered)
-  }
-
-  const addToCart = (id: number) => {
-    const filtered = products.filter((product) => product.id !== id)
-    setProducts(filtered)
-    setFilteredProducts(filtered)
-  }
-
   return (
     <div className='favorite'>
       <FavoriteActions onSort={onSort} onSearch={onSearch} />
       <div className='favorite__content'>
         <div className='favorite__products'>
           {filteredProducts.map((product) => (
-            <FavoriteCard
-              key={product.id}
-              product={product}
-              onDelete={deleteFromFavorite}
-              toCart={addToCart}
-            />
+            <FavoriteCard key={product.id} product={product} />
           ))}
         </div>
       </div>
